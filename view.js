a434f65a7178cdcc706f26d8db09d43004fc923a
Renee <97166586+reneeatho@users.noreply.github.com>
2022-03-04 11:19:01 +0100

    Add files via upload

--- view.js
+++ view.js
@@ -6,20 +6,23 @@ function show() {
   for (let i = 0; i < model.boss.length; i++) {
     html += `
     </div>
-    <div class="boss">
-    <div> ${model.boss[i].health}</div>
-    <img src="${model.boss[i].name}" class="bossSize">
-    </div>
-    
     <div class="player">
     <div> ${model.player[i].health}</div>
-    <img src="${model.player[i].name}" class="playerSize"</div>
+    <img src="${model.player[i].name}" class="playerOne"</div>
     </div>
+
+    <div class="boss">
+    <div> ${model.boss[i].health}</div>
+    <img src="${model.boss[i].name}" class="playerTwo">
+    </div>
+    
+    
   
     <img onclick="attack(${i})" class="attack" src="pizzaplain.png">
     <img onclick="reset(${i})" class="reset" src="pizzatime.png">
 `;
 }
+
 
 document.getElementById('app').innerHTML = html;
 }
